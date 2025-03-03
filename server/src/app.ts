da9a61a2a71dbfa828cfa6f5707dfb9659dd69ce
adityataank <adityataank2000@gmail.com>
2025-03-04 02:48:44 +0530

    cors config added

--- server/src/app.ts
+++ server/src/app.ts
@@ -10,11 +10,13 @@ import { v1 } from "./routes/sub-router/v1";
 import { errorMiddleware, logger } from "./middlewares/logger";
 import { authApiRequest } from "./middlewares/auth";
 
+import { CORS_CONFIG } from "./utils/constant";
+
 export const db = new PrismaClient();
 
 const app = express();
 
-app.use(cors());
+app.use(cors(CORS_CONFIG));
 
 app.use(express.json());
 
